5294191f39dec310556c123d1906c0b1bfa26bb8
andriikedys <andriy1130@ukr.net>
2018-02-18 18:29:18 +0200

    create

---
+++ src/js/app.js
@@ -0,0 +1,104 @@
+// example of simple includes for js
+
+//=include lib/jquery.min.js
+//=include lib/slick.min.js
+//=include lib/classie.js
+
+$(document).ready(function () {
+
+  $(function(f){
+    var element = f('#fix-nav-top'),
+        sectionSlide = $('.topline').height();
+    f(window).scroll(function(){
+      if($(this).scrollTop() >= sectionSlide) {
+        $('.header__nav').addClass('fixed');
+      } else{
+        $('.header__nav').removeClass('fixed');
+      }
+      // element['toggleclass'+ (f(this).scrollTop() > 200 ? 'a': 's')](500); 
+    });
+  });
+
+  $(document).on('click', '.anchorJS', scrollNav )
+
+  function scrollNav() {
+    $('html, body').animate({
+        scrollTop: $( $(this).data('href') ).offset().top
+    }, 1000);
+    return false;
+  }
+
+
+/*  Slider  */
+  
+  function startSlider() {      
+    $('.js-slider').slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      speed: 1500,
+      cssEase: 'ease',
+      autoplaySpeed: 4500,
+      slidesToShow: 1
+    });
+  };
+  startSlider();
+
+  $('.js-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.slide-item').find('.slide-item__text').removeClass('rotateIn');
+    $('.slide-item').find('.slide-item__img').removeClass('zoomIn');
+  });
+
+  $('.js-slider').on('afterChange', function(event, slick, currentSlide){
+    $('.slide-item.slick-active').find('.slide-item__text').addClass('rotateIn');
+    $('.slide-item.slick-active').find('.slide-item__img').addClass('zoomIn');
+  });
+
+/*  end Slider  */
+
+
+
+  (function() {
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+      (function() {
+        // Make sure we trim BOM and NBSP
+        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        String.prototype.trim = function() {
+          return this.replace(rtrim, '');
+        };
+      })();
+    }
+
+    [].slice.call( document.querySelectorAll( '.order-form__input' ) ).forEach( function( inputEl ) {
+      // in case the input is already filled..
+      if( inputEl.value.trim() !== '' ) {
+        classie.add( inputEl.parentNode, 'input--filled' );
+      }
+
+      // events:
+      inputEl.addEventListener( 'focus', onInputFocus );
+      inputEl.addEventListener( 'blur', onInputBlur );
+    } );
+
+    function onInputFocus( ev ) {
+      classie.add( ev.target.parentNode, 'input--filled' );
+    }
+
+    function onInputBlur( ev ) {
+      if( ev.target.value.trim() === '' ) {
+        classie.remove( ev.target.parentNode, 'input--filled' );
+      }
+    }
+  })();
+
+});	
+
+$(window).on('load', function(){
+  $(window).scroll(function() {
+    var wintop = $(window).scrollTop(), docheight = $('.content-out').height(), winheight = $(window).height();
+    var totalScroll = (wintop/(docheight-winheight))*100;
+    $(".progress-bar").css("width",totalScroll+"%");
+  });
+
+});
---
+++ src/js/lib/validate.js
@@ -0,0 +1,137 @@
+(function (parentObject) {
+    if (parentObject.validation)
+        return;
+    parentObject.validation = {
+		_defaultOptions: {
+			container: '',
+			fieldSelector: '.field-input',
+			liveCheck: true,
+			submitSelector: '.save-button',
+			submitHandler: null,
+			errorClass: 'input-error',
+			okClass: 'input-ok',
+			validators: {
+				'required': function (str) {
+					return str.length > 0;
+				},
+				'numeric': function (str) {
+					var regExp = new RegExp('\\-?\\d+((\\.|\\,)\\d{0,})?');
+					return regExp.test(str) || !str;
+				},
+				'alpha': function (str) {
+					var regExp = new RegExp('^[а-яА-ЯёЁa-zA-Z\\s]+$');
+					return regExp.test(str) || !str;
+				},
+				'alphanumeric': function (str) {
+					var regExp = new RegExp('^[а-яА-ЯёЁa-zA-Z0-9\\s]+$');
+					return regExp.test(str) || !str;
+				},
+				'date': function (str) {
+					var regExpISO = new RegExp('(19|20)\\d\\d-((0[1-9]|1[012])-(0[1-9]|[12]\\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31)'),
+						regExpRU = new RegExp('((0[1-9]|[12]\\d)\\.(0[1-9]|1[012])|30\\.(0[13-9]|1[012])|31\\.(0[13578]|1[02]))\\.(19|20)\\d\\d');
+					return (regExpISO.test(str) | regExpRU.test(str)) || !str;
+				},
+				'datetime': function (str) {
+					var regExpISO = new RegExp('(19|20)\\d\\d-((0[1-9]|1[012])-(0[1-9]|[12]\\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31) (\\d+):(\\d+)'),
+						regExpRU = new RegExp('((0[1-9]|[12]\\d)\\.(0[1-9]|1[012])|30\\.(0[13-9]|1[012])|31\\.(0[13578]|1[02]))\\.(19|20)\\d\\d (\\d+):(\\d+)');
+					return (regExpISO.test(str) | regExpRU.test(str)) || !str;
+				},
+				'time': function (str) {
+					var regExp = new RegExp('(\\d+):(\\d+)');
+					return regExp.test(str) || !str;
+				},
+				'digit': function (str) {
+					var regExp = new RegExp('^[0-9]+$');
+					return regExp.test(str) || !str;
+				},
+				'password': function (str) {
+					var regExp = new RegExp('^[а-яА-ЯёЁa-zA-Z0-9\\s]+$');
+					return regExp.test(str) || !str;
+				},
+				'email': function (str) {
+					var regExp = new RegExp('^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$');
+					return regExp.test(str) || !str;
+				},
+				'url': function (str) {
+					var regExp = new RegExp('^((https?|ftp)\\:\\/\\/)?([a-z0-9]{1})((\\.[a-z0-9-])|([a-z0-9-]))*\\.([a-z]{2,6})(\\/?)$');
+					return regExp.test(str) || !str;
+				}
+			}
+		},
+
+
+		init: function (_options) {
+			var options = $.extend({}, this._defaultOptions, (_options || {})),
+				self = this;
+			if (options.container && options.fieldSelector) {
+				if (options.submitSelector) {
+					$(options.container).find(options.submitSelector).on('click', function () {
+						if (self.isValid(options)) {
+							if (typeof (options.submitHandler) === 'function')
+								options.submitHandler();
+							return true;
+						}
+						else {
+							return false;
+						}
+					});
+				}
+				if (options.liveCheck) {
+					$(options.container).find(options.fieldSelector).each(function (cnt, item) {
+						$(item).on('click', function () {
+							self.validItem($(item), options)
+						}).on('blur', function () {
+							self.validItem($(item), options)
+						}).on('change', function () {
+							self.validItem($(item), options)
+						}).on('keyup', function () {
+							self.validItem($(item), options)
+						});
+					});
+				}
+			}
+		},
+
+		validItem: function (item, _options) {
+			var options = $.extend({}, this._defaultOptions, (_options || {})),
+				classList = $(item).attr('class').split(/\s+/),
+				validResult = true;
+			$.each(classList, function (index, cl) {
+				if (cl === 'confirmfield') {
+					validResult 
+						&= ($(options.container).find('[Name="' + $(item).attr('confirm-field') + '"]').val()
+							== $(item).val());
+				}
+				else if (typeof (options.validators[cl]) === 'function') {
+					validResult &= options.validators[cl](item.val());
+				}
+			});
+
+			if (!validResult)
+				$(item).addClass(options.errorClass).removeClass(options.okClass);
+			else
+				$(item).addClass(options.okClass).removeClass(options.errorClass);
+			return validResult;
+		},
+
+		isValid: function (_options) {
+			var options = $.extend({}, this._defaultOptions, (_options || {})),
+				validResult = true,
+				self = this;
+			if (options.container && options.fieldSelector) {
+				$(options.container).find(options.fieldSelector).each(function (cnt, item) {
+					validResult &= self.validItem($(item), options);
+				});
+			}
+			return validResult;
+		},
+
+		clear: function (_options) {
+			var options = $.extend(true, {}, this._defaultOptions, (_options || {}));
+			if (options.container && options.fieldSelector) {
+				$(options.container).find(options.fieldSelector)
+				.removeClass(options.errorClass).removeClass(options.okClass);
+			}
+		}
+	}
+})(window);
